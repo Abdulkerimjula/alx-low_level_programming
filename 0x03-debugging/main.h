6421ef082ab2e3ec71229c5cd82b808b18e823ba
abdulkerim jula <abdujula840@gmail.com>
2023-08-18 02:49:04 -0700

    you

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,7 +7,8 @@ void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
 
-void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-#endif /* MAIN_h */
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
