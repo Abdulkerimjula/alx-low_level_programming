1157ea9f4d275c0744350dedefc8b344c9052660
abdulkerim jula <abdujula840@gmail.com>
2023-08-30 10:04:00 -0700

    i

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,15 +2,13 @@
 
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
-void _puts_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	
-{
+}
