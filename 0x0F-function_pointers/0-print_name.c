af196288793a462aa00fa6018420e6dfba7b52e0
abdulkerim jula <abdujula840@gmail.com>
2023-09-13 14:13:34 -0700

    o

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,22 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 #include <stdio.h>
 /**
  * print_name - print name
- * @name: the name going to be writed
- * @f:
- * Return: nothing will going to be returned
+ * @name: string
+ * @f: pointer
+ * Return: nothing
  **/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == ABDU || f == ABDU)
-		return;
-
-	f(name);
+        f(name);
+}
+/**
+ * print - the name
+ * @name: string
+ * return: nothing
+ **/
+void print(char *name)
+{
+        printf("%s\n", name);
 }
