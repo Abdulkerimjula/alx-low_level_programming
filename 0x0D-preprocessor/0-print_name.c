6d32099fd200b4b4488ebff6a5b5d988f8ec93d2
abdulkerim jula <abdujula840@gmail.com>
2023-09-13 14:06:46 -0700

    m

--- 0x0D-preprocessor/0-print_name.c
+++ 0x0D-preprocessor/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 #include <stdio.h>
 /**
@@ -8,5 +9,14 @@
  **/
 void print_name(char *name, void (*f)(char *))
 {
-	  f(name);
+	f(name);
+}
+/**
+ * print - the name
+ * @name: string
+ * return: nothing
+ **/
+void print(char *name)
+{
+	printf("%s\n", name);
 }
