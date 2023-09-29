60cf28a7ea74136ecee4ebe1e4c02928687e440d
abdulkerim jula <abdujula840@gmail.com>
2023-09-29 11:59:59 -0700

    you

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - change the bbinary
+ * @b: string
+ * Return: number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int u;
+	unsigned int dec_val = 0;
+
+	if (!b)
+		return (0);
+
+	for (u = 0; b[u]; u++)
+	{
+		if (b[u] < '0' || b[u] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[u] - '0');
+	}
+
+	return (dec_val);
+}
